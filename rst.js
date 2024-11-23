3be318f140df8171e2537295827d3fbeb6c5232f
thaiduong2010 <132065149+thaiduong2010@users.noreply.github.com>
2024-11-23 14:02:27 +0700

    Update rst.js

--- rst.js
+++ rst.js
@@ -289,6 +289,15 @@ const rateHeaders5 = [
 { "pragma": "no-cache" },
 { "referer": "https://" + parsedTarget.host + "/" },
 ];
+function randstra(length) {
+		const characters = "0123456789";
+		let result = "";
+		const charactersLength = characters.length;
+		for (let i = 0; i < length; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
+}
 
 const a = getRandomInt(108,131);
 const b = getRandomInt(108,128);
